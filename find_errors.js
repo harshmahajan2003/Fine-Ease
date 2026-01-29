d824b39a1f5f40d9d751ac1b8e788da7f2d7982e
harshmahajan2003 <harshmahajan552@gmail.com>
2026-01-29 10:34:37 +0530

    chore: cleanup debug and diagnostic files

--- find_errors.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const results = JSON.parse(fs.readFileSync('lint_results.json', 'utf8'));
-results.filter(r => r.errorCount > 0).forEach(r => {
-    console.log(r.filePath);
-    r.messages.filter(m => m.severity === 2).forEach(m => {
-        console.log(`  ${m.line}:${m.column} ${m.message} (${m.ruleId})`);
-    });
-});
